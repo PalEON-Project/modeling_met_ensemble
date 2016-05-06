08c4f5ccd31e2871abb8dd8fe5cf9a12feb0a824
crollinson <crollinson@gmail.com>
2016-05-06 16:44:40 -0400

    fix qsub file path

--- scripts/1s_submit_get_MIROC-ESM.sh
+++ scripts/1s_submit_get_MIROC-ESM.sh
@@ -1,5 +1,5 @@
 #!bin/bash
-#$ -wd /projectnb/dietzelab/paleon/met_ensemble/
+#$ -wd /projectnb/dietzelab/paleon/met_ensemble/scripts/
 #$ -j y
 #$ -S /bin/bash
 #$ -V
@@ -7,6 +7,6 @@
 #$ -M crollinson@gmail.com
 #$ -l h_rt=24:00:00
 #$ -N MIROC-ESM
-#cd /projectnb/dietzelab/paleon/met_ensemble/
+#cd /projectnb/dietzelab/paleon/met_ensemble/scripts/
 
 sh 1_get_data_MIROC-ESM_ceda.sh
