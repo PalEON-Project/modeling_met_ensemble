41ab6392edc502cf43b30e848cb72ba74d28b770
crollinson <crollinson@gmail.com>
2016-06-02 17:01:08 -0400

    syntax, tab fixes

--- scripts/2b_extract_cruncep.sh
+++ scripts/2b_extract_cruncep.sh
@@ -23,6 +23,7 @@ dir_out=/projectnb/dietzelab/paleon/met_ensemble/data/paleon_domain
 if [ ! -d ${dir_out}/paleon1/cruncep/ ]; then
    mkdir ${dir_out}/paleon1/cruncep/
 fi
+
 if [ ! -d ${dir_out}/alaska/cruncep/ ]; then
    mkdir ${dir_out}/alaska/cruncep/
 fi
@@ -50,9 +51,8 @@ MAXLON_AK=-140
 # -----------------------------------------------------------
 # Looping through each variable!
 # -----------------------------------------------------------
-for VAR in variables
+for VAR in ${variables[@]}
 do
-
 echo ${VAR}
 
 # -----------------------------------------
@@ -71,52 +71,52 @@ for (( yr=${YR_BEG_CNCP}; yr<=${YR_END_CNCP}; yr++ )); do
    # unzip the files; doing some renaming along the way
    #  Keep in min dthat 
    if [[ ${VAR} == "psurf" ]]; then
-      cp ${dir_raw}/press/cruncep_press_${yr}.nc.gz .
-      gunzip -f cruncep_press_${yr}.nc.gz
-      mv cruncep_press_${yr}.nc cruncep_psurf_${yr}.nc
-      ncrename -v press,psurf cruncep_${VAR}_${yr}.nc
+     cp ${dir_raw}/press/cruncep_press_${yr}.nc.gz .
+     gunzip -f cruncep_press_${yr}.nc.gz
+     mv cruncep_press_${yr}.nc cruncep_psurf_${yr}.nc
+     ncrename -v press,psurf cruncep_${VAR}_${yr}.nc
    elif [[ ${VAR} == "precipf" ]]; then
-      cp ${dir_raw}/rain/cruncep_rain_${yr}.nc.gz .
-      gunzip -f cruncep_rain_${yr}.nc.gz
-      mv cruncep_rain_${yr}.nc cruncep_precipf_${yr}.nc
-      ncrename -v rain,precipf cruncep_${VAR}_${yr}.nc
+     cp ${dir_raw}/rain/cruncep_rain_${yr}.nc.gz .
+     gunzip -f cruncep_rain_${yr}.nc.gz
+     mv cruncep_rain_${yr}.nc cruncep_precipf_${yr}.nc
+     ncrename -v rain,precipf cruncep_${VAR}_${yr}.nc
    elif [[ ${VAR} == "wind" ]]; then
-      cp ${dir_raw}/uwind/cruncep_uwind_${yr}.nc.gz .
-      gunzip -f cruncep_uwind_${yr}.nc.gz
-      cp ${dir_raw}/vwind/cruncep_vwind_${yr}.nc.gz .
-      gunzip -f cruncep_vwind_${yr}.nc.gz   	
+     cp ${dir_raw}/uwind/cruncep_uwind_${yr}.nc.gz .
+     gunzip -f cruncep_uwind_${yr}.nc.gz
+     cp ${dir_raw}/vwind/cruncep_vwind_${yr}.nc.gz .
+     gunzip -f cruncep_vwind_${yr}.nc.gz     
 
-      echo "Calculating wind speed from u,v components"
-      # Copy uwind file here
-      cp cruncep_uwind_${yr}.nc.gz cruncep_${variable}_${year}.nc
-      # Add vwind to uwind file 
-      ncks -A cruncep_vwind_${yr}.nc.gz cruncep_${variable}_${year}.nc
+     echo "Calculating wind speed from u,v components"
+     # Copy uwind file here
+     cp cruncep_uwind_${yr}.nc.gz cruncep_${variable}_${year}.nc
+     # Add vwind to uwind file 
+     ncks -A cruncep_vwind_${yr}.nc.gz cruncep_${variable}_${year}.nc
 
-      # Calculate wind from u and v components
-      ncap -O -s 'Wind=abs(vwind)/sin(atan(abs(vwind/uwind)))' \
-      cruncep_${variable}_${yr}.nc cruncep_${variable}_${yr}.nc
+     # Calculate wind from u and v components
+     ncap -O -s 'Wind=abs(vwind)/sin(atan(abs(vwind/uwind)))' \
+     cruncep_${variable}_${yr}.nc cruncep_${variable}_${yr}.nc
 
-      # Exclude undesired variables
-      ncks -O -x -v uwind,vwind cruncep_${variable}_${yr}.nc \
-      cruncep_${variable}_${yr}.nc
+     # Exclude undesired variables
+     ncks -O -x -v uwind,vwind cruncep_${variable}_${yr}.nc \
+     cruncep_${variable}_${yr}.nc
 
-      # Update annotations
-      ncatted -a units,Wind,o,c,'m s-1' \
-      -a long_name,Wind,o,c,'Wind speed at the lowest atmosphere level' \
-      -a standard_name,Wind,o,c,Wind \
-      -a description,Wind,o,c,'Wind=abs(V)/sin(atan(abs(V/U)))' \
-      cruncep_${variable}_${yr}.nc
+     # Update annotations
+     ncatted -a units,Wind,o,c,'m s-1' \
+     -a long_name,Wind,o,c,'Wind speed at the lowest atmosphere level' \
+     -a standard_name,Wind,o,c,Wind \
+     -a description,Wind,o,c,'Wind=abs(V)/sin(atan(abs(V/U)))' \
+     cruncep_${variable}_${yr}.nc
    else
-      cp ${dir_raw}/${VAR}/cruncep_${VAR}_${yr}.nc.gz .
-      gunzip -f cruncep_${VAR}_${yr}.nc.gz
+     cp ${dir_raw}/${VAR}/cruncep_${VAR}_${yr}.nc.gz .
+     gunzip -f cruncep_${VAR}_${yr}.nc.gz
    fi
 
 # 
-#    # Edit NetCDF attributes to CF standard naming
-#    if [[ ${VAR} == "lwdown" || ${VAR} == "psurf"  || ${VAR} == "wind" ]]; then
-#       ncrename -d latitude,lat -v latitude,lat -d longitude,lon \
-#       -v longitude,lon cruncep_${VAR}_${yr}.nc
-#    fi
+#   # Edit NetCDF attributes to CF standard naming
+#   if [[ ${VAR} == "lwdown" || ${VAR} == "psurf"  || ${VAR} == "wind" ]]; then
+#     ncrename -d latitude,lat -v latitude,lat -d longitude,lon \
+#     -v longitude,lon cruncep_${VAR}_${yr}.nc
+#   fi
 
    # Edit NetCDF attributes to CF standard naming
    ncatted -O -a units,lon,o,c,degrees_east \
@@ -127,16 +127,16 @@ for (( yr=${YR_BEG_CNCP}; yr<=${YR_END_CNCP}; yr++ )); do
 
 
    # Parse desired geographic domain
-#    if [[ ${VAR} == "lwdown" || ${VAR} == "psurf" ]]; then
-#       # lwdown and psurf are not on same grid as other variables so
-#       #+ parse according to different grid
-#       ${CDO} \
-#       sellonlatbox,${MINLON},`echo "${MAXLON}-0.5" | bc`,${MINLAT},`echo "${MAXLAT}-0.5" | bc` \
-#       cruncep_${VAR}_${yr}.nc cruncep_${VAR}_${yr}_tmp_1.nc
-#    else
-      ${CDO} sellonlatbox,${MINLON_P1},${MAXLON_P1},${MINLAT_P1},${MAXLAT_P1} cruncep_${VAR}_${yr}.nc \
-      cruncep_${VAR}_${yr}_tmp_1.nc
-#    fi
+#   if [[ ${VAR} == "lwdown" || ${VAR} == "psurf" ]]; then
+#     # lwdown and psurf are not on same grid as other variables so
+#     #+ parse according to different grid
+#     ${CDO} \
+#     sellonlatbox,${MINLON},`echo "${MAXLON}-0.5" | bc`,${MINLAT},`echo "${MAXLAT}-0.5" | bc` \
+#     cruncep_${VAR}_${yr}.nc cruncep_${VAR}_${yr}_tmp_1.nc
+#   else
+     ${CDO} sellonlatbox,${MINLON_P1},${MAXLON_P1},${MINLAT_P1},${MAXLAT_P1} cruncep_${VAR}_${yr}.nc \
+     cruncep_${VAR}_${yr}_tmp_1.nc
+#   fi
 
    # Flipping the lat & lon for some reason
    ${CDO} invertlat cruncep_${VAR}_${yr}_tmp_1.nc \
@@ -161,19 +161,20 @@ EOF
    ${CDO} -O settaxis,${yr}-01-01,00:00,6hour cruncep_${VAR}_${yr}_tmp_3.nc \
    cruncep_${VAR}_${yr}.nc
 
-#    if [[ ${VAR} == "precipf" ]]; then
-#       ncrename -v rain,precipf cruncep_${VAR}_${yr}.nc
-#    elif [[ ${VAR} == "psurf" ]]; then
-#       ncrename -v press,psurf cruncep_${VAR}_${yr}.nc
-#    fi
+#   if [[ ${VAR} == "precipf" ]]; then
+#     ncrename -v rain,precipf cruncep_${VAR}_${yr}.nc
+#   elif [[ ${VAR} == "psurf" ]]; then
+#     ncrename -v press,psurf cruncep_${VAR}_${yr}.nc
+#   fi
 
    rm -f cruncep_${VAR}_${yr}_tmp_*.nc
 
    echo cruncep_${VAR}_${yr}.nc
 
+rm -f 0_5grid
+
 done
 
-rm -f 0_5grid
 popd
 # -----------------------------------------
 
@@ -194,53 +195,53 @@ for (( yr=${YR_BEG_CNCP}; yr<=${YR_END_CNCP}; yr++ )); do
    # unzip the files; doing some renaming along the way
    #  Keep in min dthat 
    if [[ ${VAR} == "psurf" ]]; then
-      cp ${dir_raw}/press/cruncep_press_${yr}.nc.gz .
-      gunzip -f cruncep_press_${yr}.nc.gz
-      mv cruncep_press_${yr}.nc cruncep_psurf_${yr}.nc
-      ncrename -v press,psurf cruncep_${VAR}_${yr}.nc
+     cp ${dir_raw}/press/cruncep_press_${yr}.nc.gz .
+     gunzip -f cruncep_press_${yr}.nc.gz
+     mv cruncep_press_${yr}.nc cruncep_psurf_${yr}.nc
+     ncrename -v press,psurf cruncep_${VAR}_${yr}.nc
    elif [[ ${VAR} == "precipf" ]]; then
-      cp ${dir_raw}/rain/cruncep_rain_${yr}.nc.gz .
-      gunzip -f cruncep_rain_${yr}.nc.gz
-      mv cruncep_rain_${yr}.nc cruncep_precipf_${yr}.nc
-      ncrename -v rain,precipf cruncep_${VAR}_${yr}.nc
+     cp ${dir_raw}/rain/cruncep_rain_${yr}.nc.gz .
+     gunzip -f cruncep_rain_${yr}.nc.gz
+     mv cruncep_rain_${yr}.nc cruncep_precipf_${yr}.nc
+     ncrename -v rain,precipf cruncep_${VAR}_${yr}.nc
    elif [[ ${VAR} == "wind" ]]; then
-      cp ${dir_raw}/uwind/cruncep_uwind_${yr}.nc.gz .
-      gunzip -f cruncep_uwind_${yr}.nc.gz
-      cp ${dir_raw}/vwind/cruncep_vwind_${yr}.nc.gz .
-      gunzip -f cruncep_vwind_${yr}.nc.gz   	
+     cp ${dir_raw}/uwind/cruncep_uwind_${yr}.nc.gz .
+     gunzip -f cruncep_uwind_${yr}.nc.gz
+     cp ${dir_raw}/vwind/cruncep_vwind_${yr}.nc.gz .
+     gunzip -f cruncep_vwind_${yr}.nc.gz     
 
-      echo "Calculating wind speed from u,v components"
-      # Copy uwind file here
-      cp cruncep_uwind_${yr}.nc.gz cruncep_${variable}_${year}.nc
-      # Add vwind to uwind file 
-      ncks -A cruncep_vwind_${yr}.nc.gz cruncep_${variable}_${year}.nc
+     echo "Calculating wind speed from u,v components"
+     # Copy uwind file here
+     cp cruncep_uwind_${yr}.nc.gz cruncep_${variable}_${year}.nc
+     # Add vwind to uwind file 
+     ncks -A cruncep_vwind_${yr}.nc.gz cruncep_${variable}_${year}.nc
 
-      # Calculate wind from u and v components
-      ncap -O -s 'Wind=abs(vwind)/sin(atan(abs(vwind/uwind)))' \
-      cruncep_${variable}_${yr}.nc cruncep_${variable}_${yr}.nc
+     # Calculate wind from u and v components
+     ncap -O -s 'Wind=abs(vwind)/sin(atan(abs(vwind/uwind)))' \
+     cruncep_${variable}_${yr}.nc cruncep_${variable}_${yr}.nc
 
-      # Exclude undesired variables
-      ncks -O -x -v uwind,vwind cruncep_${variable}_${yr}.nc \
-      cruncep_${variable}_${yr}.nc
+     # Exclude undesired variables
+     ncks -O -x -v uwind,vwind cruncep_${variable}_${yr}.nc \
+     cruncep_${variable}_${yr}.nc
 
-      # Update annotations
-      ncatted -a units,Wind,o,c,'m s-1' \
-      -a long_name,Wind,o,c,'Wind speed at the lowest atmosphere level' \
-      -a standard_name,Wind,o,c,Wind \
-      -a description,Wind,o,c,'Wind=abs(V)/sin(atan(abs(V/U)))' \
-      cruncep_${variable}_${yr}.nc
+     # Update annotations
+     ncatted -a units,Wind,o,c,'m s-1' \
+     -a long_name,Wind,o,c,'Wind speed at the lowest atmosphere level' \
+     -a standard_name,Wind,o,c,Wind \
+     -a description,Wind,o,c,'Wind=abs(V)/sin(atan(abs(V/U)))' \
+     cruncep_${variable}_${yr}.nc
 
    else
-      cp ${dir_raw}/${VAR}/cruncep_${VAR}_${yr}.nc.gz .
-      gunzip -f cruncep_${VAR}_${yr}.nc.gz
+     cp ${dir_raw}/${VAR}/cruncep_${VAR}_${yr}.nc.gz .
+     gunzip -f cruncep_${VAR}_${yr}.nc.gz
    fi
 
 # 
-#    # Edit NetCDF attributes to CF standard naming
-#    if [[ ${VAR} == "lwdown" || ${VAR} == "psurf"  || ${VAR} == "wind" ]]; then
-#       ncrename -d latitude,lat -v latitude,lat -d longitude,lon \
-#       -v longitude,lon cruncep_${VAR}_${yr}.nc
-#    fi
+#   # Edit NetCDF attributes to CF standard naming
+#   if [[ ${VAR} == "lwdown" || ${VAR} == "psurf"  || ${VAR} == "wind" ]]; then
+#     ncrename -d latitude,lat -v latitude,lat -d longitude,lon \
+#     -v longitude,lon cruncep_${VAR}_${yr}.nc
+#   fi
 
    # Edit NetCDF attributes to CF standard naming
    ncatted -O -a units,lon,o,c,degrees_east \
@@ -251,16 +252,16 @@ for (( yr=${YR_BEG_CNCP}; yr<=${YR_END_CNCP}; yr++ )); do
 
 
    # Parse desired geographic domain
-#    if [[ ${VAR} == "lwdown" || ${VAR} == "psurf" ]]; then
-#       # lwdown and psurf are not on same grid as other variables so
-#       #+ parse according to different grid
-#       ${CDO} \
-#       sellonlatbox,${MINLON},`echo "${MAXLON}-0.5" | bc`,${MINLAT},`echo "${MAXLAT}-0.5" | bc` \
-#       cruncep_${VAR}_${yr}.nc cruncep_${VAR}_${yr}_tmp_1.nc
-#    else
-      ${CDO} sellonlatbox,${MINLON_AK},${MAXLON_AK},${MINLAT_AK},${MAXLAT_AK} cruncep_${VAR}_${yr}.nc \
-      cruncep_${VAR}_${yr}_tmp_1.nc
-#    fi
+#   if [[ ${VAR} == "lwdown" || ${VAR} == "psurf" ]]; then
+#     # lwdown and psurf are not on same grid as other variables so
+#     #+ parse according to different grid
+#     ${CDO} \
+#     sellonlatbox,${MINLON},`echo "${MAXLON}-0.5" | bc`,${MINLAT},`echo "${MAXLAT}-0.5" | bc` \
+#     cruncep_${VAR}_${yr}.nc cruncep_${VAR}_${yr}_tmp_1.nc
+#   else
+     ${CDO} sellonlatbox,${MINLON_AK},${MAXLON_AK},${MINLAT_AK},${MAXLAT_AK} cruncep_${VAR}_${yr}.nc \
+     cruncep_${VAR}_${yr}_tmp_1.nc
+#   fi
 
    # Flipping the lat & lon for some reason
    ${CDO} invertlat cruncep_${VAR}_${yr}_tmp_1.nc \
@@ -285,23 +286,25 @@ EOF
    ${CDO} -O settaxis,${yr}-01-01,00:00,6hour cruncep_${VAR}_${yr}_tmp_3.nc \
    cruncep_${VAR}_${yr}.nc
 
-#    if [[ ${VAR} == "precipf" ]]; then
-#       ncrename -v rain,precipf cruncep_${VAR}_${yr}.nc
-#    elif [[ ${VAR} == "psurf" ]]; then
-#       ncrename -v press,psurf cruncep_${VAR}_${yr}.nc
-#    fi
+#   if [[ ${VAR} == "precipf" ]]; then
+#     ncrename -v rain,precipf cruncep_${VAR}_${yr}.nc
+#   elif [[ ${VAR} == "psurf" ]]; then
+#     ncrename -v press,psurf cruncep_${VAR}_${yr}.nc
+#   fi
 
    rm -f cruncep_${VAR}_${yr}_tmp_*.nc
 
    echo cruncep_${VAR}_${yr}.nc
 
+rm -f 0_5grid
+
 done
 
-rm -f 0_5grid
 popd
 # -----------------------------------------
 
 
-end do
+done
 # -----------------------------------------------------------
+
  
