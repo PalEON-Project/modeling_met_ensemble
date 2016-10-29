acca88ede5ac706566e6a6a2b069301a0503f8eb
crollinson <crollinson@gmail.com>
2016-10-29 16:10:56 -0400

    Initial workflow with .nc files, but these files are corrupted
    Something apparently went wrong when writing the .nc files for each
    ensemble member and everything except tmax is empty.  For the sake of
    time, we’re going to work with the .Rdata file that I thankfully saved
    instead. (That will be in the next commit)

--- scripts/4_predict_subdaily_met.R
+++ scripts/4_predict_subdaily_met.R
@@ -40,11 +40,66 @@
 # -----------------------------------
 # 0. Load libraries, set up file paths, etc
 # -----------------------------------
-# -----------------------------------
+# Script to prototype temporal downscaling
+library(ncdf4)
+library(mgcv)
+library(MASS)
+# library(lubridate)
+library(ggplot2)
+# library(tictoc)
+rm(list=ls())
 
 
-# -----------------------------------
-# 1. Load bias-correction file
+dir.base <- "/projectnb/dietzelab/paleon/met_ensemble/data/met_ensembles/HARVARD/"
+# mod.out <- "../data/met_ensembles/HARVARD/subday_models"
+# mod.out <- "~/Desktop/met_ensembles/HARVARD/subday_models"
+# fig.dir <- file.path(mod.out, "model_qaqc")
+
+# if(!dir.exists(mod.out)) dir.create(mod.out, recursive = T)
+# if(!dir.exists(fig.dir)) dir.create(fig.dir, recursive = T)
+
+GCM.list = c("CCSM4", "MIROC-ESM", "MPI-ESM-P", "bcc-csm1-1")
 # -----------------------------------
 
-# -----------------------------------
+for(GCM in GCM.list){
+  path.gcm <- file.path(dir.base, GCM, "day")
+  setwd(path.gcm)
+  # All of the daily ensembles should be zipped to save space
+  ens.day <- dir(".", ".tar.bz2")
+  
+  for(i in 1:length(ens.day)){
+    # Extract the base name of this ensemble member
+    ens.name <- substr(ens.day[i], 1, nchar(ens.day[i])-8)
+
+    # uncompress the ensemble member
+    system(paste0("tar -jxvf ", ens.day[i])) 
+    
+    # Get list of files
+    files.day <- dir(ens.name, ".nc")
+    
+    # -----------------------------------
+    # 1. Load bias-correction file
+    # NOTE: Need to start with the last and work to the first
+    # -----------------------------------
+    for(j in length(files.day):1){
+      ncT <- nc_open(file.path(ens.name, files.day[j]))
+      
+      dat.nc <- data.frame(time        = ncvar_get(ncT, "time"   ),
+                           tmax.day    = ncvar_get(ncT, "tmax"   ),
+                           tmin.day    = ncvar_get(ncT, "tmin"   ),
+                           precipf.day = ncvar_get(ncT, "precipf"),
+                           swdown.day  = ncvar_get(ncT, "swdown" ),
+                           lwdown.day  = ncvar_get(ncT, "lwdown" ),
+                           qair.day    = ncvar_get(ncT, "qair"   ),
+                           press.day   = ncvar_get(ncT, "press"  ),
+                           wind.day    = ncvar_get(ncT, "wind"   )
+                           )
+      nc_close(ncT)
+      
+      dat.nc$date <- date(dat.nc$time, )
+      
+    }
+    # -----------------------------------
+      
+  }
+}
