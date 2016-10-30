37f50308963ec825b78d47913e9bdef2c51d43a4
crollinson <crollinson@gmail.com>
2016-10-29 23:12:36 -0400

    add qsub script

---
+++ scripts/4s_submit_predict_subdaily_met.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+#$ -wd /projectnb/dietzelab/paleon/met_ensemble/scripts/
+#$ -j y
+#$ -S /bin/bash
+#$ -V
+#$ -m e
+#$ -q "geo*"
+#$ -M crollinson@gmail.com
+#$ -l h_rt=120:00:00
+#$ -N predict_diel
+#cd /projectnb/dietzelab/paleon/met_ensemble/scripts/
+
+R CMD BATCH 4_predict_subdaily_met.R
