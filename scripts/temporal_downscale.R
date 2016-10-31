6f5755f50556a7c24631655e3a3470a612b44483
crollinson <crollinson@gmail.com>
2016-10-31 08:10:53 -0400

    minor tweaks while trying to find a bug
    turns out the bug was that I forgot to generate new qair models after
    adding tmax.day as a predictor.

--- scripts/temporal_downscale.R
+++ scripts/temporal_downscale.R
@@ -39,7 +39,7 @@ predict.subdaily <- function(dat.mod, n.ens, path.model, lags.init, dat.train){
     
     # Load the meta info for the betas
     betas.swdown <- nc_open(file.path(path.model,"betas_swdown.nc"))
-    n.betas <- nrow(ncvar_get(betas.swdown, "1"))
+    n.beta <- nrow(ncvar_get(betas.swdown, "1"))
     
     dat.sim[["swdown"]] <- data.frame(array(dim=c(nrow(dat.mod), n.ens)))
 
@@ -53,13 +53,16 @@ predict.subdaily <- function(dat.mod, n.ens, path.model, lags.init, dat.train){
       rows.now = which(dat.mod$time.day==i)
       rows.mod = which(dat.mod$time.day==i & dat.mod$hour %in% hrs.day)
       
-      dat.temp <- dat.mod[rows.mod,c("time.day", "year", "doy", "hour", "tmax.day", "tmin.day", "precipf.day", "swdown.day", "lwdown.day", "press.day", "qair.day", "wind.day")]
-      dat.temp$swdown = 99999 # Dummy value so there's a column
-
-      rows.beta <- sample(1:n.betas, n.ens, replace=T)
-      Rbeta <- as.matrix(ncvar_get(betas.swdown, paste(day.now))[rows.beta,], nrow=length(rows.beta), ncol=ncol(betas))
-      dimnames(Rbeta)[[2]] <- names(coef(mod.swdown.doy[[paste(day.now)]]))
+      dat.temp <- dat.mod[rows.mod,c("time.day", "year", "doy", "hour", 
+                                     "tmax.day", "tmin.day", "precipf.day", "swdown.day", "lwdown.day", "press.day", "qair.day", "wind.day",
+                                     "next.tmax", "next.tmin", "next.precipf", "next.swdown", "next.lwdown", "next.press", "next.qair", "next.wind")]
       
+      dat.temp$swdown = 99999 # Dummy value so there's a column
+      day.now = unique(dat.temp$doy)
+      
+      rows.beta <- sample(1:n.beta, n.ens, replace=T)
+      Rbeta <- as.matrix(ncvar_get(betas.swdown, paste(day.now))[rows.beta,], nrow=length(rows.beta), ncol=ncol(betas))
+
       dat.pred <- predict.met(newdata=dat.temp, 
                               model.predict=mod.swdown.doy[[paste(day.now)]], 
                               Rbeta=Rbeta, 
@@ -69,7 +72,7 @@ predict.subdaily <- function(dat.mod, n.ens, path.model, lags.init, dat.train){
                               n.ens=n.ens)
       
       dat.pred[dat.pred<0] <- 0
-
+      
       # Randomly pick which values to save & propogate
       cols.prop <- sample(1:n.ens, ncol(dat.sim$swdown), replace=T)
       for(j in 1:ncol(dat.sim$swdown)){
@@ -96,7 +99,7 @@ predict.subdaily <- function(dat.mod, n.ens, path.model, lags.init, dat.train){
     
     # Load the meta info for the betas
     betas.tair <- nc_open(file.path(path.model,"betas_tair.nc"))
-    n.betas <- nrow(ncvar_get(betas.tair, "1"))
+    n.beta <- nrow(ncvar_get(betas.tair, "1"))
     
     dat.sim[["tair"]] <- data.frame(array(dim=c(nrow(dat.mod), n.ens)))
 
@@ -105,7 +108,7 @@ predict.subdaily <- function(dat.mod, n.ens, path.model, lags.init, dat.train){
       dat.temp <- dat.mod[rows.now,c("time.day", "year", "doy", "hour", 
                                      "tmax.day", "tmin.day", "precipf.day", "swdown.day", "lwdown.day", "press.day", "qair.day", "wind.day",
                                      "next.tmax", "next.tmin", "next.precipf", "next.swdown", "next.lwdown", "next.press", "next.qair", "next.wind")]
-      dat.temp$tair = 999999 # Dummy value so there's a column
+      dat.temp$tair = 99999 # Dummy value so there's a column
       day.now = unique(dat.temp$doy)
       
       # Set up the lags
@@ -125,9 +128,8 @@ predict.subdaily <- function(dat.mod, n.ens, path.model, lags.init, dat.train){
       
       # dat.temp$swdown <- stack(dat.sim$swdown[rows.now,])[,1]
       
-      rows.beta <- sample(1:n.betas, n.ens, replace=T)
+      rows.beta <- sample(1:n.beta, n.ens, replace=T)
       Rbeta <- as.matrix(ncvar_get(betas.tair, paste(day.now))[rows.beta,], nrow=length(rows.beta), ncol=ncol(betas))
-      dimnames(Rbeta)[[2]] <- names(coef(mod.tair.doy[[paste(day.now)]]))
       
       dat.pred <- predict.met(newdata=dat.temp, 
                               model.predict=mod.tair.doy[[paste(day.now)]], 
@@ -139,7 +141,7 @@ predict.subdaily <- function(dat.mod, n.ens, path.model, lags.init, dat.train){
       
       # Randomly pick which values to save & propogate
       cols.prop <- sample(1:n.ens, ncol(dat.sim$tair), replace=T)
-
+      
       for(j in 1:ncol(dat.sim$tair)){
         dat.sim[["tair"]][rows.now,j] <- dat.pred[dat.temp$ens==paste0("X", j),cols.prop[j]]
       }
@@ -166,7 +168,7 @@ predict.subdaily <- function(dat.mod, n.ens, path.model, lags.init, dat.train){
     
     # Load the meta info for the betas
     betas.precipf <- nc_open(file.path(path.model,"betas_precipf.nc"))
-    n.betas <- nrow(ncvar_get(betas.precipf, "1"))
+    n.beta <- nrow(ncvar_get(betas.precipf, "1"))
     
     dat.sim[["precipf"]] <- data.frame(array(dim=c(nrow(dat.mod), n.ens)))
     
@@ -174,15 +176,24 @@ predict.subdaily <- function(dat.mod, n.ens, path.model, lags.init, dat.train){
       rows.now = which(dat.mod$time.day==i)
       dat.temp <- dat.mod[rows.now,c("time.day", "year", "doy", "hour", 
                                      "tmax.day", "tmin.day", "precipf.day", "swdown.day", "lwdown.day", "press.day", "qair.day", "wind.day",
-                                     "next.tmax", "next.tmin", "next.precipf", "next.swdown", "next.lwdown", "next.press")]
+                                     "next.tmax", "next.tmin", "next.precipf", "next.swdown", "next.lwdown", "next.press", "next.qair", "next.wind")]
       dat.temp$precipf = 99999 # Dummy value so there's a column
       dat.temp$rain.prop = 99999 # Dummy value so there's a column
-      
       day.now = unique(dat.temp$doy)
       
-      rows.beta <- sample(1:n.betas, n.ens, replace=T)
+      # Set up the lags
+      if(i==max(dat.mod$time.day)){
+        sim.lag <- stack(lags.init$precipf)
+        names(sim.lag) <- c("lag.precipf", "ens")
+      } else {
+        sim.lag <- stack(data.frame(array(dat.sim[["precipf"]][dat.mod$time.day==(i+1)  & dat.mod$hour==0,], dim=c(1, ncol(dat.sim$precipf)))))
+        names(sim.lag) <- c("lag.precipf", "ens")
+      }
+      dat.temp <- merge(dat.temp, sim.lag, all.x=T)
+      
+      
+      rows.beta <- sample(1:n.beta, n.ens, replace=T)
       Rbeta <- as.matrix(ncvar_get(betas.precipf, paste(day.now))[rows.beta,], nrow=length(rows.beta), ncol=ncol(betas))
-      # dimnames(Rbeta)[[2]] <- names(coef(mod.precipf.doy[[paste(day.now)]]))[1:ncol(Rbeta)]
       
       dat.pred <- predict.met(newdata=dat.temp, 
                               model.predict=mod.precipf.doy[[paste(day.now)]], 
@@ -213,8 +224,9 @@ predict.subdaily <- function(dat.mod, n.ens, path.model, lags.init, dat.train){
       
       # Randomly pick which values to save & propogate
       cols.prop <- sample(1:n.ens, ncol(dat.sim$precipf), replace=T)
+      
       for(j in 1:ncol(dat.sim$precipf)){
-        dat.sim[["precipf"]][rows.now,j] <- dat.pred[,cols.prop[j]]
+        dat.sim[["precipf"]][rows.now,j] <- dat.pred[dat.temp$ens==paste0("X", j),cols.prop[j]]
       }
     }
     
@@ -235,7 +247,7 @@ predict.subdaily <- function(dat.mod, n.ens, path.model, lags.init, dat.train){
     
     # Load the meta info for the betas
     betas.lwdown <- nc_open(file.path(path.model,"betas_lwdown.nc"))
-    n.betas <- nrow(ncvar_get(betas.lwdown, "1"))
+    n.beta <- nrow(ncvar_get(betas.lwdown, "1"))
     
     dat.sim[["lwdown"]] <- data.frame(array(dim=c(nrow(dat.mod), n.ens)))
 
@@ -243,14 +255,15 @@ predict.subdaily <- function(dat.mod, n.ens, path.model, lags.init, dat.train){
       rows.now = which(dat.mod$time.day==i)
       dat.temp <- dat.mod[rows.now,c("time.day", "year", "doy", "hour", 
                                      "tmax.day", "tmin.day", "precipf.day", "swdown.day", "lwdown.day", "press.day", "qair.day", "wind.day",
-                                     "next.tmax", "next.tmin", "next.precipf", "next.swdown", "next.lwdown", "next.press")]
-      dat.temp$lwdown = 999999 # Dummy value so there's a column
+                                     "next.tmax", "next.tmin", "next.precipf", "next.swdown", "next.lwdown", "next.press", "next.qair", "next.wind")]
+      dat.temp$lwdown = 99999 # Dummy value so there's a column
       day.now = unique(dat.temp$doy)
       
       # Set up the lags
       if(i==max(dat.mod$time.day)){
         sim.lag <- stack(lags.init$lwdown)
         names(sim.lag) <- c("lag.lwdown", "ens")
+        
       } else {
         sim.lag <- stack(data.frame(array(dat.sim[["lwdown"]][dat.mod$time.day==(i+1)  & dat.mod$hour==0,], dim=c(1, ncol(dat.sim$lwdown)))))
         names(sim.lag) <- c("lag.lwdown", "ens")
@@ -258,9 +271,8 @@ predict.subdaily <- function(dat.mod, n.ens, path.model, lags.init, dat.train){
       dat.temp <- merge(dat.temp, sim.lag, all.x=T)
       
       # dat.temp$swdown <- stack(dat.sim$swdown[rows.now,])[,1]
-      # dat.temp$tair <- stack(dat.sim$tair[rows.now,])[,1]
       
-      rows.beta <- sample(1:n.betas, n.ens, replace=T)
+      rows.beta <- sample(1:n.beta, n.ens, replace=T)
       Rbeta <- as.matrix(ncvar_get(betas.lwdown, paste(day.now))[rows.beta,], nrow=length(rows.beta), ncol=ncol(betas))
 
       dat.pred <- predict.met(newdata=dat.temp, 
@@ -271,13 +283,13 @@ predict.subdaily <- function(dat.mod, n.ens, path.model, lags.init, dat.train){
                               Rbeta.resid=NULL, 
                               n.ens=n.ens)
       dat.pred <- dat.pred^2 # because squared to prevent negative numbers
-
+      
       # Randomly pick which values to save & propogate
       cols.prop <- sample(1:n.ens, ncol(dat.sim$lwdown), replace=T)
+      
       for(j in 1:ncol(dat.sim$lwdown)){
         dat.sim[["lwdown"]][rows.now,j] <- dat.pred[dat.temp$ens==paste0("X", j),cols.prop[j]]
       }
-      
     }
     
     nc_close(betas.lwdown)
@@ -296,7 +308,7 @@ predict.subdaily <- function(dat.mod, n.ens, path.model, lags.init, dat.train){
     
     # Load the meta info for the betas
     betas.press <- nc_open(file.path(path.model,"betas_press.nc"))
-    n.betas <- nrow(ncvar_get(betas.press, "1"))
+    n.beta <- nrow(ncvar_get(betas.press, "1"))
     
     dat.sim[["press"]] <- data.frame(array(dim=c(nrow(dat.mod), n.ens)))
 
@@ -304,21 +316,22 @@ predict.subdaily <- function(dat.mod, n.ens, path.model, lags.init, dat.train){
       rows.now = which(dat.mod$time.day==i)
       dat.temp <- dat.mod[rows.now,c("time.day", "year", "doy", "hour", 
                                      "tmax.day", "tmin.day", "precipf.day", "swdown.day", "lwdown.day", "press.day", "qair.day", "wind.day",
-                                     "next.tmax", "next.tmin", "next.precipf", "next.swdown", "next.lwdown", "next.press")]
-      dat.temp$press = 999999 # Dummy value so there's a column
+                                     "next.tmax", "next.tmin", "next.precipf", "next.swdown", "next.lwdown", "next.press", "next.qair", "next.wind")]
+      dat.temp$press = 99999 # Dummy value so there's a column
       day.now = unique(dat.temp$doy)
       
       # Set up the lags
       if(i==max(dat.mod$time.day)){
         sim.lag <- stack(lags.init$press)
         names(sim.lag) <- c("lag.press", "ens")
+        
       } else {
         sim.lag <- stack(data.frame(array(dat.sim[["press"]][dat.mod$time.day==(i+1)  & dat.mod$hour==0,], dim=c(1, ncol(dat.sim$press)))))
         names(sim.lag) <- c("lag.press", "ens")
       }
       dat.temp <- merge(dat.temp, sim.lag, all.x=T)
       
-      rows.beta <- sample(1:n.betas, n.ens, replace=T)
+      rows.beta <- sample(1:n.beta, n.ens, replace=T)
       Rbeta <- as.matrix(ncvar_get(betas.press, paste(day.now))[rows.beta,], nrow=length(rows.beta), ncol=ncol(betas))
 
       dat.pred <- predict.met(newdata=dat.temp, 
@@ -328,13 +341,13 @@ predict.subdaily <- function(dat.mod, n.ens, path.model, lags.init, dat.train){
                               model.resid=NULL, 
                               Rbeta.resid=NULL, 
                               n.ens=n.ens)
-
+      
       # Randomly pick which values to save & propogate
       cols.prop <- sample(1:n.ens, ncol(dat.sim$press), replace=T)
+      
       for(j in 1:ncol(dat.sim$press)){
         dat.sim[["press"]][rows.now,j] <- dat.pred[dat.temp$ens==paste0("X", j),cols.prop[j]]
       }
-      
     }
 
     nc_close(betas.press)
@@ -354,7 +367,7 @@ predict.subdaily <- function(dat.mod, n.ens, path.model, lags.init, dat.train){
     
     # Load the meta info for the betas
     betas.qair <- nc_open(file.path(path.model,"betas_qair.nc"))
-    n.betas <- nrow(ncvar_get(betas.qair, "1"))
+    n.beta <- nrow(ncvar_get(betas.qair, "1"))
     
     dat.sim[["qair"]] <- data.frame(array(dim=c(nrow(dat.mod), n.ens)))
 
@@ -370,16 +383,15 @@ predict.subdaily <- function(dat.mod, n.ens, path.model, lags.init, dat.train){
       if(i==max(dat.mod$time.day)){
         sim.lag <- stack(lags.init$qair)
         names(sim.lag) <- c("lag.qair", "ens")
-        
       } else {
         sim.lag <- stack(data.frame(array(dat.sim[["qair"]][dat.mod$time.day==(i+1)  & dat.mod$hour==0,], dim=c(1, ncol(dat.sim$qair)))))
         names(sim.lag) <- c("lag.qair", "ens")
       }
       dat.temp <- merge(dat.temp, sim.lag, all.x=T)
       
-      rows.beta <- sample(1:n.betas, n.ens, replace=T)
+      rows.beta <- sample(1:n.beta, n.ens, replace=T)
       Rbeta <- as.matrix(ncvar_get(betas.qair, paste(day.now))[rows.beta,], nrow=length(rows.beta), ncol=ncol(betas))
-
+      
       dat.pred <- predict.met(newdata=dat.temp, 
                               model.predict=mod.qair.doy[[paste(day.now)]], 
                               Rbeta=Rbeta, 
@@ -387,7 +399,6 @@ predict.subdaily <- function(dat.mod, n.ens, path.model, lags.init, dat.train){
                               model.resid=NULL, 
                               Rbeta.resid=NULL, 
                               n.ens=n.ens)
-      
       dat.pred <- exp(dat.pred) # because log-transformed
       
       # having a *very* hard time keeping humidity reasonable, even with transformations so truncating the max
@@ -395,10 +406,10 @@ predict.subdaily <- function(dat.mod, n.ens, path.model, lags.init, dat.train){
       
       # Randomly pick which values to save & propogate
       cols.prop <- sample(1:n.ens, ncol(dat.sim$qair), replace=T)
+      
       for(j in 1:ncol(dat.sim$qair)){
         dat.sim[["qair"]][rows.now,j] <- dat.pred[dat.temp$ens==paste0("X", j),cols.prop[j]]
       }
-      
     }
 
     nc_close(betas.qair)
@@ -417,7 +428,7 @@ predict.subdaily <- function(dat.mod, n.ens, path.model, lags.init, dat.train){
     
     # Load the meta info for the betas
     betas.wind <- nc_open(file.path(path.model,"betas_wind.nc"))
-    n.betas <- nrow(ncvar_get(betas.wind, "1"))
+    n.beta <- nrow(ncvar_get(betas.wind, "1"))
     
     dat.sim[["wind"]] <- data.frame(array(dim=c(nrow(dat.mod), n.ens)))
 
@@ -439,7 +450,7 @@ predict.subdaily <- function(dat.mod, n.ens, path.model, lags.init, dat.train){
       }
       dat.temp <- merge(dat.temp, sim.lag, all.x=T)
       
-      rows.beta <- sample(1:n.betas, n.ens, replace=T)
+      rows.beta <- sample(1:n.beta, n.ens, replace=T)
       Rbeta <- as.matrix(ncvar_get(betas.wind, paste(day.now))[rows.beta,], nrow=length(rows.beta), ncol=ncol(betas))
 
       dat.pred <- predict.met(newdata=dat.temp, 
@@ -453,6 +464,7 @@ predict.subdaily <- function(dat.mod, n.ens, path.model, lags.init, dat.train){
       
       # Randomly pick which values to save & propogate
       cols.prop <- sample(1:n.ens, ncol(dat.sim$wind), replace=T)
+      
       for(j in 1:ncol(dat.sim$wind)){
         dat.sim[["wind"]][rows.now,j] <- dat.pred[dat.temp$ens==paste0("X", j),cols.prop[j]]
       }
