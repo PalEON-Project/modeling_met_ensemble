a0a098cf646bde51803033addfe8857d317ad708
Drew Duckett <duckettdj@g.cofc.edu>
2017-02-06 09:52:40 -0600

    Replaced R Markdown with R Script
    Should work now without needing R Studio. Also cleaned up code and added
    more comments

--- scripts/ssurgo_awc_summary.R
+++ scripts/ssurgo_awc_summary.R
@@ -33,89 +33,9 @@
 
 
 
-
-New SSURGO code which does not require fips codes to obtain the data
-
-
-```{r}
-area_extent <- extent(-72.50, -72.00, 42.50, 43.00) #create extent object
-area_polygon <- polygon_from_extent(area_extent, proj4string = "+proj=longlat +datum=WGS84") #turn extent into polygon
-ssurgo_extent <- get_ssurgo(template = area_polygon, label = "harvard", extraction.dir = paste0("./EXTRACTIONS/", label, "/SSURGO")) #download ssurgo data for polygon
-```
-
-```{r}
-#This will not work due to chunk below
-area_list <- list()
-p = 1
-for (polygon in ssurgo_extent[["spatial"]]@polygons){
-  poly_area <- ssurgo_extent[["spatial"]]@polygons[[p]]@area
-  area_list <- append(area_list, poly_area, after = length(area_list))
-  p = p + 1
-}
-```
-
-```{r}
-#some polygon lists contain more than 1 polygon
-n = 1
-x = 1
-for (polygon in ssurgo_extent[["spatial"]]@polygons){
-  if (length(ssurgo_extent[["spatial"]]@polygons[[x]]@Polygons) > 1){
-    n = n + 1
-  }
-  x = x + 1
-}
-```
-
-```{r}
-ssurgo_shape <- readOGR(dsn = "harvard_SSURGO_Mapunits.shp") #import shape file. will need to change directory
-area_list <- gArea(ssurgo_shape, byid = TRUE) #calculate area for each polygon in shape file
-ssurgo_projection <- ssurgo_shape #copy shape in order to do projection
-ssurgo_projection <- spTransform(ssurgo_projection, CRS = CRS("+proj=aea +ellps=WGS84 +datum=WGS84 +units=m")) #project using albers equal area
-area_list <- gArea(ssurgo_projection, byid = TRUE) #calculate area for each polygon
-```
-
-```{r}
-area_mukey <- area_list #make a copy
-names(area_mukey) <- ssurgo_extent[["spatial"]]$MUKEY #name list indices as mukeys
-area_sum <- sum(area_list) #sum entire area
-area_prop <- area_list / area_sum
-```
-
-
-```{r}
-awc <- ssurgo_extent[["tabular"]]$muaggatt$aws025wta #extract all awc values
-names(awc) <- ssurgo_extent[["tabular"]]$muaggatt$mukey #name list indices as mukeys
-area_weight <- awc * area_prop
-```
-
-
-```{r}
-test_list <- list() #create empty list
-m = 1 #set counter
-for (mukey in unique(ssurgo_extent[["spatial"]]$MUKEY)){ #for each unique mukey
-  
-  index_list <- which(names(area_mukey) == mukey) #create list of lists where each list contains indices for each unique mukey
-  test_list[m] <- list(area_mukey[index_list]) #create list of lists where each list contains all area measurements for a single mukey
-  
-  m = m + 1
-}
-names(test_list) <- unique(ssurgo_extent[["spatial"]]$MUKEY) #name list indices as mukeys
-```
-
-```{r}
-list_sums <- sapply(test_list, sum) #sum each list (calculate total area for each unique mukey)
-list_prop <- list_sums / area_sum #get proportion of area for each mukey
-```
-
-```{r}
-awc_final <- sum((awc * list_prop), na.rm = TRUE) #weight awc values by proportion of area and sum to get weighted average
-```
-
-
-```{r}
 ssurgo_extraction_v5 <- function(lat_min, lat_max, lon_min, lon_max, label){
   
-  #load necessary libraries
+  #load required libraries
   library(FedData)
   library(raster)
   library(rgdal)
@@ -139,35 +59,31 @@ ssurgo_extraction_v5 <- function(lat_min, lat_max, lon_min, lon_max, label){
   #project data to albers equal area and calculate polygon areas
   ssurgo_shape <- spTransform(ssurgo_shape, CRS = CRS("+proj=aea +ellps=WGS84 +datum=WGS84 +units=m")) #project using albers equal area - need to match proj4string to original shapefile except for +proj
   area_list <- gArea(ssurgo_shape, byid = TRUE) #calculate area for each polygon
-  
   names(area_list) <- ssurgo_extent[["spatial"]]$MUKEY #name list indices as mukeys
+  
+  #calculate total area
   area_sum <- sum(area_list) #sum entire area
   
-  awc <- ssurgo_extent[["tabular"]]$muaggatt$aws025wta #extract all awc values
-  names(awc) <- ssurgo_extent[["tabular"]]$muaggatt$mukey #name list indices as mukeys
-  
+  #match areas to mukeys
   test_list <- list() #create empty list
   m = 1 #set counter
   for (mukey in unique(ssurgo_extent[["spatial"]]$MUKEY)){ #for each unique mukey
-  
+    
     index_list <- which(names(area_list) == mukey) #create list of lists where each list contains indices for each unique mukey
     test_list[m] <- list(area_list[index_list]) #create list of lists where each list contains all area measurements for a single mukey
-  
+    
     m = m + 1
   }
   names(test_list) <- unique(ssurgo_extent[["spatial"]]$MUKEY) #name list indices as mukeys
   
+  #calculate area of each mukey and proportion of area compared to total area
   list_sums <- sapply(test_list, sum) #sum each list (calculate total area for each unique mukey)
   list_prop <- list_sums / area_sum #get proportion of area for each mukey
   
+  #extract awc values and calculate weighted average
+  awc <- ssurgo_extent[["tabular"]]$muaggatt$aws025wta #extract all awc values
+  names(awc) <- ssurgo_extent[["tabular"]]$muaggatt$mukey #name list indices as mukeys
   awc_final <- sum((awc * list_prop), na.rm = TRUE) #weight awc values by proportion of area and sum to get weighted average
   
   return(awc_final)
 }
-```
-
-
-```{r}
-ssurgo_extraction_v5(45.00, 45.50, -69.0, -68.50, "howland")
-```
-
