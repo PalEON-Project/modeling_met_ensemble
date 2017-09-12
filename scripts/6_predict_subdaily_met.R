86600cd54eaffc2a0d813e3fe3ce52f8c899123e
Christy Rollinson <crollinson@gmail.com>
2017-09-12 16:09:35 -0500

    Run temporal downscaling
    **NOTE: be *very* careful if using less than a 7-day window.  This
    causes LOTS of noise in your hour-to-hour trends**

---
+++ scripts/6_predict_subdaily_met.R
@@ -0,0 +1,116 @@
+# -----------------------------------
+# Script Information
+# -----------------------------------
+# Purpose: Create statistical models to predict subdaily meteorology from daily means
+# Creator: Christy Rollinson, 7 September 2017
+# Contact: crollinson@gmail.com
+# -----------------------------------
+
+# -----------------------------------
+# Description
+# -----------------------------------
+# Apply the statistical models from step 3 to convert the daily, bias-corrected met 
+# files from step 2 (daily means) and predict subdaily values.  This gets done by 
+# filtering backwards in time starting with the present (where the trianing data is).
+#
+# There are ways to improve this and speed it up, but hopefully this works for now.
+# We whould also probably think about applying this filter approach to the bias-
+# correction step to avoid abrupt and unreasonable jumps in climate.
+# -----------------------------------
+
+
+# -----------------------------------
+# Workflow
+# -----------------------------------
+# 0. Load libraries, set up file paths, etc
+# ----- Loop through by ensemble member by year ----------
+#    1. Use align.met to match temporal resolution to training data
+#    2. Predict subdaily values, filtering backwards in time
+#    3. Write annual output into .nc files 
+#       - separate file for each year/ensemle member; 
+#       - all met vars in one annual file (similar to pecan met structure)
+# ----- recycle steps 1 - 3 for all years in file ----------
+# -----------------------------------
+
+
+# -----------------------------------
+# 0. Load libraries, set up file paths, etc
+# -----------------------------------
+# Script to prototype temporal downscaling
+library(ncdf4)
+library(mgcv)
+library(MASS)
+library(lubridate)
+library(ggplot2)
+library(stringr)
+library(tictoc)
+library(parallel)
+# library(tictoc)
+rm(list=ls())
+
+# wd.base <- "~/met_ensemble/"
+# wd.base <- "~/Desktop/Research/PalEON_CR/met_ensemble/"
+# setwd(wd.base)
+
+dat.base <- "~/Desktop/Research/met_ensembles/data"
+path.pecan <- "~/Desktop/Research/pecan"
+
+path.train <- file.path(dat.base, "paleon_sites/HARVARD/NLDAS")
+path.lm <- file.path(dat.base, "met_ensembles/HARVARD.v4/1hr/mods.tdm")
+path.in <- file.path(dat.base, "met_ensembles/HARVARD.v4/day/ensembles")
+path.out <- file.path(dat.base, "met_ensembles/HARVARD.v4/1hr/ensembles")
+
+# Hard-coding numbers for Harvard
+# site.name="TEST"
+site.lat=42.54
+site.lon=-72.18
+# 
+# GCM.list = c("CCSM4", "MIROC-ESM", "MPI-ESM-P", "bcc-csm1-1")
+GCM.list = "MIROC-ESM"
+ens.hr  <- 3 # Number of hourly ensemble members to create
+n.day <- 10 # Number of daily ensemble members to process
+yrs.plot <- c(2015, 1985, 1920, 1875, 1800, 1000, 850)
+timestep="1hr"
+# years.sim=2015:1900
+years.sim=NULL
+cores.max = 2
+
+# Set up the appropriate seed
+set.seed(0017)
+seed.vec <- sample.int(1e6, size=500, replace=F)
+# -----------------------------------
+
+# -----------------------------------
+# 2. Apply the model
+# -----------------------------------
+source(file.path(path.pecan, "modules/data.atmosphere/R", "tdm_predict_subdaily_met.R"))
+source(file.path(path.pecan, "modules/data.atmosphere/R", "tdm_lm_ensemble_sims.R"))
+source(file.path(path.pecan, "modules/data.atmosphere/R", "align_met.R"))
+source(file.path(path.pecan, "modules/data.atmosphere/R", "tdm_subdaily_pred.R"))
+# source(file.path(path.pecan, "tdm_predict_subdaily_met.R"))
+
+# Set & create the output directory
+if(!dir.exists(path.out)) dir.create(path.out, recursive=T)
+
+for(GCM in GCM.list){
+  # GCM="Ameriflux"
+  # tic()
+  # Set the directory where the output is & load the file
+  path.gcm <- file.path(path.in, GCM)
+  
+  out.ens <- file.path(path.out, GCM)
+  
+  # Doing this one ensemble member at at time
+  gcm.members <- dir(path.gcm)
+  for(ens.now in gcm.members){
+    predict_subdaily_met(outfolder=out.ens, in.path=file.path(path.in, GCM, ens.now), 
+                         in.prefix=ens.now, lm.models.base=path.lm, 
+                         path.train=path.train, direction.filter="backwards", yrs.predict=1900:2015, cores.max = 12, 
+                         ens.labs = 1:3, resids = FALSE, parallel = FALSE, n.cores = NULL, 
+                         overwrite = FALSE, seed=seed.vec[1], print.progress = TRUE)
+  }
+  # dat.day <- dir(path.gcm, ".Rdata")
+  # load(file.path(path.gcm, dat.day)) # Loads dat.out.full
+  
+}
+# -----------------------------------
