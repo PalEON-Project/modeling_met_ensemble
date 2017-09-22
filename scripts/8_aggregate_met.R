85ebcf8ea8b320ad215e93067b1f8954a4b5cd33
Christy Rollinson <crollinson@gmail.com>
2017-09-22 15:22:22 -0500

    Implement aggregation parallelization
    Split the workflow function (aggregate.met) and the actual function
    that does the aggregation (aggreate.file).  This seems redundant and I
    could probably just have a function within a function, but I don’t like
    that.

--- scripts/8_aggregate_met.R
+++ scripts/8_aggregate_met.R
@@ -15,14 +15,26 @@
 # 1. 
 # -----------------------------------
 # rm(list=ls())
+source("aggregate_met.R")
+source("aggregate_file.R")
 
-path.in = "~/Desktop/Research/met_ensembles/data/met_ensembles/HARVARD/1hr/ensembles/CCSM4/CCSM4_001.01/"
-years.agg=NULL
-save.day=T
-save.month=T
-out.base = "~/Desktop/Research/met_ensembles/data/met_ensembles/HARVARD/"
-day.dir="day/TEST1" 
-mo.dir="month/TEST1"
-add.vars=c("daylength", "temp.max", "temp.min")
-parallel=F
-n.cores=NULL
+in.base = "~/Desktop/Research/met_ensembles/data/met_ensembles/HARVARD/1hr/ensembles/"
+out.base = "~/Desktop/Research/met_ensembles/data/met_ensembles/HARVARD/aggregated"
+
+GCM.list <- dir(in.base)
+for(GCM in GCM.list){
+  gcm.ens <- dir(file.path(in.base, GCM))
+  pb <- txtProgressBar(min=0, max=length(gcm.ens), style=3)
+  pb.ind=1
+  for(ens in gcm.ens){
+    aggregate.met(path.in=file.path(in.base, GCM, ens), 
+                  years.agg=NULL, save.day=T, save.month=T, 
+                  out.base=out.base, day.dir=file.path("day", GCM, ens), mo.dir=file.path("month", GCM, ens), 
+                  add.vars=c("daylength", "air_temperature_maximum", "air_temperature_minimum"),
+                  parallel=T, n.cores=8, 
+                  print.progress=F, verbose=FALSE)
+    
+    setTxtProgressBar(pb, pb.ind)
+    pb.ind=pb.ind+1
+  }
+}
