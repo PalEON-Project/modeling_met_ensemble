e5b606d7c76034f7e04874387e9d9483b51d33fb
Christy Rollinson <crollinson@gmail.com>
2018-09-21 16:16:01 -0500

    updated file paths

--- scripts/11_Randomized_MetOrder.R
+++ scripts/11_Randomized_MetOrder.R
@@ -5,12 +5,12 @@ vers=".v1"
 site.lat  = 45.54127
 site.lon  = -95.5313
 
-# met.all <- read.csv(file.path("/Volumes/GoogleDrive/My Drive/PalEON_Met_Ensembles/data/", paste(site, vers, sep="."), "/aggregated/month/PDSI_AllMembers.csv"))
-met.all <- read.csv("~/met_ensemble/data/met_ensembles/", paste(site, vers, sep="."), "/aggregated/month/PDSI_AllMembers.csv")
+met.all <- read.csv(file.path("/Volumes/GoogleDrive/My Drive/PalEON_Met_Ensembles/data/", paste0(site.name, vers), "/aggregated/month/PDSI_AllMembers.csv"))
+# met.all <- read.csv("~/met_ensemble/data/met_ensembles/", paste0(site.name, vers), "/aggregated/month/PDSI_AllMembers.csv")
 ens.names <- names(met.all[,2:ncol(met.all)])
 
 ens.out <- data.frame(order=1:length(ens.names), ensemble=sample(ens.names, replace=F))
-# write.csv(ens.out, file.path("/Volumes/GoogleDrive/My Drive/PalEON_Met_Ensembles/data/", paste(site, vers, sep="."), "/EnsembleOrder.csv"), row.names=F)
-write.csv(ens.out, file.path("~/met_ensemble/data/met_ensembles/", paste(site, vers, sep="."), "/EnsembleOrder.csv"), row.names=F)
+write.csv(ens.out, file.path("/Volumes/GoogleDrive/My Drive/PalEON_Met_Ensembles/data", paste0(site.name, vers), "EnsembleOrder.csv"), row.names=F)
+# write.csv(ens.out, file.path("~/met_ensemble/data/met_ensembles", paste(site.name, vers), "EnsembleOrder.csv"), row.names=F)
 
 head(ens.out)
